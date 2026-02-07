8829e6780f304d1e91e55465e69f428f871ddc10
Sivakrishna <shv.krish@gmail.com>
2026-02-07 17:23:51 +0530

    debug: minimal service worker sanity check

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,8 @@
 // public/sw.js
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
